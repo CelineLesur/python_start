db0dc85bc47ecff18583acce44384b7f23b351b4
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2023-08-05 12:37:33 +0200

    modif indice

--- fc_min_max.py
+++ fc_min_max.py
@@ -1,18 +1,18 @@
-i=4
-j=15
+indice, i=4
+indice, j=15
 
 # fonction minimum
 if i<j:
   return i
-  print ("le minimum est") i
+  print ("le minimum est", i)
 else
   return j
-  print ("le minimum est") j
+  print ("le minimum est"), j
   
 # fonction maximum
 if i>j:
   return i
-  print ("le maximum est") i
+  print "le maximum est", i
 else
   return j
-  print ("le maximum est") j
+  print ("le maximum est"), j
