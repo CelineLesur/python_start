3e6f99a01d953e84590f8bccc1d6c4a26682c98c
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2023-08-05 12:22:09 +0200

    Create fc_min_max.py

---
+++ fc_min_max.py
@@ -0,0 +1,18 @@
+i=4
+j=15
+
+# fonction minimum
+if i<j:
+  return i
+  print ("le minimum est") i
+else
+  return j
+  print ("le minimum est") j
+  
+# fonction maximum
+if i>j:
+  return i
+  print ("le maximum est") i
+else
+  return j
+  print ("le maximum est") j
